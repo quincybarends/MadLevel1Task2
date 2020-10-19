c165e065c879ba4e5f6e32cc28fdfc66b7c39b56
quincyba <qbarend@gmail.com>
2020-10-19 11:18:22 +0200

    Check user input & only allow T or F

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnSubmit.setOnClickListener{
+            onSubmit()
+        }
+    }
+
+    private fun onSubmit(){
+        var correctAnswers = 0
+        val answerTT = binding.etTT.text.toString()
+        val answerTF = binding.etTF.text.toString()
+        val answerFT = binding.etFT.text.toString()
+        val answerFF = binding.etFF.text.toString()
+
+        if  (answerTT == "T"){
+            correctAnswers += 1
+        }
+        if (answerTF == "F"){
+            correctAnswers += 1
+        }
+        if (answerFT == "F"){
+            correctAnswers += 1
+        }
+        if (answerFF == "F"){
+            correctAnswers += 1
+        }
+
+        Toast.makeText(this, getString(R.string.correct_answers, correctAnswers), Toast.LENGTH_LONG).show()
     }
 }
